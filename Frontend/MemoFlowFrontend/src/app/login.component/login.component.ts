09ad61231e13f453d2e5b05c4e43c147f11977e1
Adriano995 <adriano.incognito@icloud.com>
2025-06-23 11:46:41 +0200

    Start Frontend
    Inizializzazione Angular.
    
    Creazione form login hardcoded.
    Funzionante.

---
+++ Frontend/MemoFlowFrontend/src/app/login.component/login.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../auth.service'; // <<-- importa il servizio
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService // <<-- inject del servizio
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+onSubmit(): void {
+  console.log('Form valido?', this.loginForm.valid);
+  console.log('Valori form:', this.loginForm.value);
+  if (this.loginForm.valid) {
+    const { email, password } = this.loginForm.value;
+    const success = this.authService.login(email, password);
+
+    if (success) {
+      console.log('Login riuscito!');
+    } else {
+      console.log('Credenziali errate!');
+    }
+  } else {
+    console.log('Form non valido');
+  }
+}
+
+}
---
+++ Frontend/MemoFlowFrontend/src/main.ts
@@ -0,0 +1,17 @@
+import 'zone.js';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(ReactiveFormsModule)
+  ]
+});
