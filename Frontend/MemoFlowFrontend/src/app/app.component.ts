c5b6438b13371e4df3024c4306829cb5a4e1daaf
Adriano995 <adriano.incognito@icloud.com>
2025-07-27 11:29:55 +0200

    Setup evento
    Creazione componente Evento base

--- Frontend/MemoFlowFrontend/src/app/app.component.ts
+++ Frontend/MemoFlowFrontend/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ThemeService } from './theme';
 import { UserComponent } from "./user/user.component";
+import { Evento } from "./evento-component/evento.component";
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,8 @@ import { UserComponent } from "./user/user.component";
   imports: [
     RouterOutlet,
     CommonModule,
-    UserComponent
+    UserComponent,
+    Evento
 ],
   providers: [ThemeService], 
   templateUrl: './app.component.html',
