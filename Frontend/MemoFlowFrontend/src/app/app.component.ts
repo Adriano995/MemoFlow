c907159203f60ed860008ba5f61fa8b95bad4124
Adriano995 <adriano.incognito@icloud.com>
2025-07-30 13:21:13 +0200

    Update app.component.ts
    cambiati import per evitare conflitti

--- Frontend/MemoFlowFrontend/src/app/app.component.ts
+++ Frontend/MemoFlowFrontend/src/app/app.component.ts
@@ -3,17 +3,21 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ThemeService } from './theme';
 import { UserComponent } from "./user/user.component";
+import { RouterModule, Router } from '@angular/router';
+import { AuthService } from './auth/auth.service';
 import { EventoCreazioneComponent } from './evento-creazione/evento-creazione';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    CommonModule,
-    UserComponent,
-    EventoCreazioneComponent
+imports: [
+  RouterOutlet,
+  CommonModule,
+  RouterModule,
+  UserComponent,
+  EventoCreazioneComponent
 ],
+
   providers: [ThemeService], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
