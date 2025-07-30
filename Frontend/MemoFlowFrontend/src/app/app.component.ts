2fd5d64e44024c5c1c46a2bd68bc2121f6485614
CiccioTonio02 <adefidio@studenti.laziodigital.it>
2025-07-30 12:39:44 +0200

    Aggiornamento crededenziali/anagrafica/logout
    Da inserire CSS, aggiornare modifica profilo

--- Frontend/MemoFlowFrontend/src/app/app.component.ts
+++ Frontend/MemoFlowFrontend/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ThemeService } from './theme';
 import { UserComponent } from "./user/user.component";
+import { RouterModule, Router } from '@angular/router';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,7 @@ import { UserComponent } from "./user/user.component";
   imports: [
     RouterOutlet,
     CommonModule,
+    RouterModule,
     UserComponent
 ],
   providers: [ThemeService], 
@@ -19,8 +22,10 @@ import { UserComponent } from "./user/user.component";
 export class AppComponent implements OnInit {
   title = 'MemoFlowFrontend';
 
-  constructor(private themeService: ThemeService) {}
+  utenteId = 1;
 
+  constructor(private themeService: ThemeService,private authService: AuthService,private router: Router) {}
+  
   ngOnInit(): void {
   }
 
@@ -30,5 +35,23 @@ export class AppComponent implements OnInit {
 
   isLightTheme(): boolean {
     return this.themeService.getCurrentTheme() === 'light';
+  }
+
+  isLoggedIn(): boolean {
+    return this.authService.isAuthenticated();
+  }
+
+  get userId(): number | null {
+    return this.authService.getUserId();
+  }
+
+  shouldShowNavbar(): boolean {
+    const hiddenRoutes = ['/login', '/register'];
+    return this.isLoggedIn() && !hiddenRoutes.includes(this.router.url);
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
   }
 }
